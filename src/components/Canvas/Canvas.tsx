33cafa0adc7ef2ba1ef419253020aaf8da4a15bc
Dany Boza <db@backium.co>
2021-05-19 18:17:14 -0500

    contex menu support #54

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useCanvasContext } from '@components/Canvas/hooks'
 import { fabric } from 'fabric'
 import {
@@ -9,10 +9,23 @@ import {
   useGuidelinesHandler,
   usePanningHandler,
 } from '@components/Canvas/handlers'
+import { Box } from '@chakra-ui/react'
+// import styled from ""
+//@ts-ignore
+fabric.util.addListener(document.getElementsByClassName('upper-canvas')[0], 'contextmenu', function (e) {
+  e.preventDefault()
+})
+
+// const ContextMenuContainer = styled
 
 function Canvas() {
   const containerRef = useContainerHandler()
   const { setCanvas } = useCanvasContext()
+  const [contextMenu, setContextMenu] = useState({
+    visible: false,
+    left: 0,
+    top: 0,
+  })
   useCustomizationHandler()
   useGuidelinesHandler()
   useEventsHandler()
@@ -21,14 +34,37 @@ function Canvas() {
   useEffect(() => {
     const initialHeigh = containerRef.current.clientHeight
     const initialWidth = containerRef.current.clientWidth
+    //Disable context menu
+    //@ts-ignore
 
     const canvas = new fabric.Canvas('canvas', {
       backgroundColor: '#ecf0f1',
       height: initialHeigh,
       width: initialWidth,
+      fireRightClick: true,
     })
 
+    //Disable context menu
+    //@ts-ignore
+    fabric.util.addListener(document.getElementsByClassName('upper-canvas')[0], 'contextmenu', function (e) {
+      e.preventDefault()
+    })
+
+    canvas.on('mouse:down', e => {
+      // console.log(e)
+      //@ts-ignore
+      e.e.preventDefault()
+      // console.log(e)
+      if (e.button === 3) {
+        console.log(e)
+        // @ts-ignore
+        updateContextMenu({ ...contextMenu, visible: true, left: e.e.offsetX, top: e.e.offsetY })
+      } else {
+        updateContextMenu({ visible: false })
+      }
+    })
     setCanvas(canvas)
+
     const workarea = new fabric.Rect({
       //@ts-ignore
       id: 'workarea',
@@ -44,8 +80,30 @@ function Canvas() {
 
     workarea.center()
   }, [])
+
+  const updateContextMenu = updatedValues => {
+    setContextMenu({ ...contextMenu, ...updatedValues })
+  }
   return (
     <div style={{ flex: 1, display: 'flex', position: 'relative' }}>
+      {contextMenu.visible && (
+        <Box
+          className="A"
+          style={{
+            position: 'absolute',
+            left: `${contextMenu.left}px`,
+            top: `${contextMenu.top}px`,
+            zIndex: 9,
+            width: '240px',
+            pointerEvents: 'none',
+          }}
+        >
+          <div>Alonso</div>
+          <div>Dany</div>
+          <div>Lee</div>
+          {/* Hello */}
+        </Box>
+      )}
       <div style={{ width: '100%', height: '100%', position: 'absolute' }}>
         <div style={{ height: '100%' }} className="editor-canvas" ref={containerRef}>
           <canvas id="canvas"></canvas>
